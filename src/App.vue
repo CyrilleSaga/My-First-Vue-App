457024d9d1d9cbde1146cb9ad9e38c0be2e9e34c
CyrilleSaga <cyrilletoumi8@gmail.com>
2022-03-28 14:27:28 +0100

    Installation du package VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
-    <div id="app">
-        <nav>
-            <NavLink url="/" text="Accueil" />
-            <NavLink url="/contact" text="Contact" />
-            <NavLink url="/about" text="A propos" />
-        </nav>
-    </div>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import NavLink from './components/NavLink.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavLink
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
@@ -26,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
